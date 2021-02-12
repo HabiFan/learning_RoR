b024f398115b48178f9fe247735d873088d08748
Fanis <habfanis@mail.ru>
2021-02-12 12:36:49 +0300

    Add lesson_1

---
+++ lesson_1/area_triangle.rb
@@ -0,0 +1,7 @@
+
+puts "Введите основание треугольника: "
+a = gets.chomp
+puts "Введите высоту треугольника: "
+h = gets.chomp
+square_triangle = 0.5 * a.to_i * h.to_i
+puts "Площадь треуголника равен: #{square_triangle}"
